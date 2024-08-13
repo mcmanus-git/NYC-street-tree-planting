8983ab10ae39c09bf384cccd416cde72618bd532
Michael McManus <melancholic_maclean@yahoo.com>
2024-08-12 23:19:52 -0400

    update etl pipeline for new nyc data location

--- pipelines/tree_data_etl.py
+++ pipelines/tree_data_etl.py
@@ -1,33 +1,43 @@
 import pandas as pd
-from urllib import request
+from requests import get
 from datetime import datetime
 import os
 
 
-def get_most_recent_upload_date():
-    """
-    Fetches most recent data update from NYC Street Tree Planting csv dataset via url
-    :return: datetime object YYYY-MM-DD HH:MM:SS - most recent date NYC Street Tree Planting data was updated
-    """
-    r = request.urlopen('https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv')
-
-    last_build_date = r.readlines()[5].decode('utf-8').split(',')[1].strip('\n')
-
-    last_build_date = datetime.strptime(last_build_date.strip('"'), '%Y-%m-%d %H:%M:%S')
-
-    return last_build_date
-
-
 def download_new_data():
-    """
-    Downloads new csv dataset from url and returns clean version in Pandas DataFrame
-    :return: Pandas DataFrame
-    """
-    df = pd.read_csv('https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv', skiprows=7)
-    df['CompletedDate'] = pd.to_datetime(df['CompletedDate'])
-    df['PlantingSeason'] = pd.to_datetime(df['PlantingSeason'])
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
-    return df
+    url = 'https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv'
+    r = get(url, headers=headers)
+    return r.text
+
+
+def get_most_recent_upload_date(in_response_text: str) -> datetime.date:
+    last_update = datetime.strptime(in_response_text.replace('"', '').split('\n')[5].split(',')[1], '%Y-%m-%d %H:%M:%S').date()
+    return last_update
+
+
+def process_new_data(in_response_text: str):
+    # Split the text into lines
+    lines = in_response_text.replace('"', '').split('\n')
+
+    # Extract the column names from line 7
+    columns = lines[7].split(',')
+
+    # Extract the data starting from line 8
+    data = [line.split(',') for line in lines[8:] if line]
+
+    # Create a DataFrame using the column names and data
+    out_df = pd.DataFrame(data, columns=columns)
+
+    out_df['CompletedDate'] = pd.to_datetime(out_df['CompletedDate'])
+    out_df['PlantingSeason'] = pd.to_datetime(out_df['PlantingSeason'])
+    out_df['lng'] = out_df['lng'].astype(float)
+    out_df['lat'] = out_df['lat'].astype(float)
+
+    return out_df
 
 
 def load_tree_data(save=True, return_data=True):
@@ -39,30 +49,104 @@ def load_tree_data(save=True, return_data=True):
     :param return_data: Default=True return Pandas DataFrame object
     :return: Pandas DataFrame
     """
-    today = datetime.now()
+    today = datetime.now().date()
     today_file = f'data/street_tree_planting_{today.strftime("%Y_%m_%d")}.pkl'
 
     if os.path.exists(today_file):
         df = pd.read_pickle(today_file)
+        return df
 
-    elif not os.path.exists(today_file):
-        most_recent_web = get_most_recent_upload_date()
-        if most_recent_web < today:
+    else:
+        web_data = download_new_data()
+        most_recent_web = get_most_recent_upload_date(web_data)
+        if most_recent_web == today:
+            df = process_new_data(web_data)
+            if save:
+                df.to_pickle(today_file)
+            return df
+        elif most_recent_web < today:
             most_recent_file = f'data/street_tree_planting_{most_recent_web.strftime("%Y_%m_%d")}.pkl'
             if os.path.exists(most_recent_file):
                 df = pd.read_pickle(most_recent_file)
-            elif not os.path.exists(most_recent_file):
-                df = download_new_data()
+            else:
+                print('Processing New Data')
+                df = process_new_data(web_data)
                 if save:
                     df.to_pickle(f'data/street_tree_planting_{most_recent_web.strftime("%Y_%m_%d")}.pkl')
-        elif most_recent_web == today:
-            df = get_most_recent_upload_date()
-            if save:
-                df.to_pickle(today_file)
-
-    if return_data:
-        return df
+            return df
 
 
 if __name__ == '__main__':
     load_tree_data()
+
+
+# import pandas as pd
+# from urllib import request
+# from datetime import datetime
+# import os
+#
+#
+# def get_most_recent_upload_date():
+#     """
+#     Fetches most recent data update from NYC Street Tree Planting csv dataset via url
+#     :return: datetime object YYYY-MM-DD HH:MM:SS - most recent date NYC Street Tree Planting data was updated
+#     """
+#     r = request.urlopen('https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv')
+#     # https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv
+#     # https://www.nycgovparks.org/trees/street-tree-planting/locations#:~:text=a%20particular%20address.-,Download%20The%C2%A0Data,-Show
+#
+#     last_build_date = r.readlines()[5].decode('utf-8').split(',')[1].strip('\n')
+#
+#     last_build_date = datetime.strptime(last_build_date.strip('"'), '%Y-%m-%d %H:%M:%S')
+#
+#     return last_build_date
+#
+#
+# def download_new_data():
+#     """
+#     Downloads new csv dataset from url and returns clean version in Pandas DataFrame
+#     :return: Pandas DataFrame
+#     """
+#     df = pd.read_csv('https://www.nycgovparks.org/tree-work-orders/street_tree_planting.csv', skiprows=7)
+#     df['CompletedDate'] = pd.to_datetime(df['CompletedDate'])
+#     df['PlantingSeason'] = pd.to_datetime(df['PlantingSeason'])
+#
+#     return df
+#
+#
+# def load_tree_data(save=True, return_data=True):
+#     """
+#     Determines whether most recent dataset available has already been retrieved.
+#         If True: Fetches Pickle;
+#         If False: ETL New Data from URL
+#     :param save: Default=True save DataFrame to Pickle once Retrieved and Cleaned
+#     :param return_data: Default=True return Pandas DataFrame object
+#     :return: Pandas DataFrame
+#     """
+#     today = datetime.now()
+#     today_file = f'data/street_tree_planting_{today.strftime("%Y_%m_%d")}.pkl'
+#
+#     if os.path.exists(today_file):
+#         df = pd.read_pickle(today_file)
+#
+#     elif not os.path.exists(today_file):
+#         most_recent_web = get_most_recent_upload_date()
+#         if most_recent_web < today:
+#             most_recent_file = f'data/street_tree_planting_{most_recent_web.strftime("%Y_%m_%d")}.pkl'
+#             if os.path.exists(most_recent_file):
+#                 df = pd.read_pickle(most_recent_file)
+#             elif not os.path.exists(most_recent_file):
+#                 df = download_new_data()
+#                 if save:
+#                     df.to_pickle(f'data/street_tree_planting_{most_recent_web.strftime("%Y_%m_%d")}.pkl')
+#         elif most_recent_web == today:
+#             df = get_most_recent_upload_date()
+#             if save:
+#                 df.to_pickle(today_file)
+#
+#     if return_data:
+#         return df
+#
+#
+# if __name__ == '__main__':
+#     load_tree_data()
